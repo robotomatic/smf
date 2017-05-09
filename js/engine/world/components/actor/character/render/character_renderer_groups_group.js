f348b3ea926644861666d1f303f6c65812741b52
robotomatic <dennis.r.hoskins@gmail.com>
2017-05-09 00:30:04 -0400

    Cleaned-up render path
    A little cleaner...

--- js/engine/world/components/actor/character/render/character_renderer_groups_group.js
+++ js/engine/world/components/actor/character/render/character_renderer_groups_group.js
@@ -7,52 +7,52 @@ function CharacterRendererGroupsGroup() {
 }
 
 CharacterRendererGroupsGroup.prototype.renderGroup = function(gamecanvas, character, groupdef, groupnames, group, debugrects, debug) {
-
     var gp = groupdef ? groupdef.path : "";
     this.renderer.buildPolygon(gp, groupnames, group);
     if (!this.renderer.shouldDraw()) return false;
-
     var gg = groupdef ? groupdef : group;
     if (gg.draw == false) return false;
-    
     var dodebug = debug.character || debug.guts;
     var color = dodebug ? "white" : this.getColor(gamecanvas, character, gg.color);
-    
     var linkpath = this.renderer.hasLinkPath();
     if (gg.link && gg.link != "skip") {
-        if (!linkpath) {
-            this.renderer.startLinkPath(color, gg.linktype);
-            return false;
-        } else if (gg.link && gg.linktype == "end") {
-            if (linkpath && gg.link != "skip") {
-                this.renderer.addLinkPath();
-                this.renderer.endLinkPath(gamecanvas);
-                return true;
-            }
-        } else if (gg.link && linkpath) {
-            this.renderer.addLinkPath();
-            return false;
-        }
+        return this.renderGroupLinkPath(gamecanvas, linkpath, color, gg);
     } else {
-
         gamecanvas.setFillStyle(color);
         gamecanvas.beginPath();
-
         this.renderer.drawPolygon(gg.path, gamecanvas);
-        
         if (gg.clip) {
-            if (gg.clip == "start") {
-                this.renderer.startClipPath(gamecanvas);
-            } else if (gg.clip == "end") {
-                this.renderer.endClipPath(gamecanvas);
-            }
+            this.renderGroupLinkPath(gamecanvas, gg);
         }
     } 
-    
     gamecanvas.commit();
     return true;
 }
 
+CharacterRendererGroupsGroup.prototype.renderGroupLinkPath = function(gamecanvas, linkpath, color, gg) {
+    if (!linkpath) {
+        this.renderer.startLinkPath(color, gg.linktype);
+        return false;
+    } else if (gg.link && gg.linktype == "end") {
+        if (linkpath && gg.link != "skip") {
+            this.renderer.addLinkPath();
+            this.renderer.endLinkPath(gamecanvas);
+            return true;
+        }
+    } else if (gg.link && linkpath) {
+        this.renderer.addLinkPath();
+        return false;
+    }
+    return false;
+}
+
+CharacterRendererGroupsGroup.prototype.renderGroupClipPath = function(gamecanvas, gg) {
+    if (gg.clip == "start") {
+        this.renderer.startClipPath(gamecanvas);
+    } else if (gg.clip == "end") {
+        this.renderer.endClipPath(gamecanvas);
+    }
+}
 
 CharacterRendererGroupsGroup.prototype.getColor = function(gamecanvas, character, colorname) {
     this.gradient = false;
